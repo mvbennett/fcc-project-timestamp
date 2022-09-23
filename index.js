5e48609fc72360464de7f5ab47589a81cf56d0f2
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 12:32:44 +0900

    api is working with both date and unix inputs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,21 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date', (req, res) => {
 
+  if(req.params.date.match(/\d{4}-\d{2}-\d{2}/)) {
+    res.json({unix: new Date(req.params.date).getTime() / 1000, utc: new Date(req.params.date).toGMTString()});
+  } else {
+    res.json({unix: parseInt(req.params.date), utc: new Date(parseInt(req.params.date)).toGMTString()})
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
